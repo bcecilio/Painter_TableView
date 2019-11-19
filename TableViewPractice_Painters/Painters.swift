08c9cc367738cf35753d711ba3b8d0aa3a21820b
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-18 22:12:45 -0500

    Initial Commit

---
+++ TableViewPractice_Painters/Painters.swift
@@ -0,0 +1,26 @@
+//
+//  Painters.swift
+//  TableViewPractice_Painters
+//
+//  Created by Brendon Cecilio on 11/18/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import Foundation
+
+struct Painters {
+    var name: String
+    var description: String
+    var era: String
+    
+    static let painters = [
+    Painters(name: "Edwin Austin Abbey", description: "Edwin Austin Abbey RA (April 1, 1852 – August 1, 1911) was an American muralist, illustrator, and painter. He flourished at the beginning of what is now referred to as the \"golden age\" of illustration, and is best known for his drawings and paintings of Shakespearean and Victorian subjects, as well as for his painting of Edward VII's coronation.[1][2][3] His most famous set of murals, The Quest and Achievement of the Holy Grail, adorns the Boston Public Library.", era: "Golden Age"),
+    Painters(name: "Vincent Van Gogh", description: "Vincent Willem van Gogh was a Dutch post-impressionist painter who is among the most famous and influential figures in the history of Western art. In just over a decade he created about 2,100 artworks, including around 860 oil paintings, most of which date from the last two years of his life. They include landscapes, still lifes, portraits and self-portraits, and are characterised by bold colours and dramatic, impulsive and expressive brushwork that contributed to the foundations of modern art. He was not commercially successful, and his suicide at 37 came after years of mental illness and poverty.", era: "Post-Impressionist"),
+    Painters(name: "Jesper Ejsing", description: "Jesper Ejsing was born in Denmark in 1973. He first discovered fantasy through the works of Tolkien and got introduced to D&D on Christmas Day, 1986. Skipping through the pages of the rulebooks, he set a goal for the rest of his life: He would become a fantasy artist. He would make a living illustrating things that live only in imagination... one way or the other.", era: "Present"),
+    Painters(name: "Donato Giancola", description: "Donato Giancola (born 1967) is an American artist specializing in narrative realism with science fiction and fantasy content.", era: "Present"),
+    Painters(name: "John Singer Sargant", description: "John Singer Sargent (/ˈsɑːrdʒənt/; January 12, 1856 – April 14, 1925)[1] was an American expatriate artist, considered the \"leading portrait painter of his generation\" for his evocations of Edwardian-era luxury.[2][3] He created roughly 900 oil paintings and more than 2,000 watercolors, as well as countless sketches and charcoal drawings. \nFrom the beginning, Sargent's work is characterized by remarkable technical facility, particularly in his ability to draw with a brush, which in later years inspired admiration as well as criticism for a supposed superficiality. His commissioned works were consistent with the grand manner of portraiture, while his informal studies and landscape paintings displayed a familiarity with Impressionism.", era: "Realism"),
+    Painters(name: "Francisco de Goya", description: "(born March 30, 1746, Fuendetodos, Spain—died April 16, 1828, Bordeaux, France), Spanish artist whose paintings, drawings, and engravings reflected contemporary historical upheavals and influenced important 19th- and 20th-century painters. The series of etchings The Disasters of War (1810–14) records the horrors of the Napoleonic invasion. His masterpieces in painting include The Naked Maja, The Clothed Maja (c. 1800–05), and The 3rd of May 1808: The Execution of the Defenders of Madrid (1814).", era: "Baroque/Rococo"),
+    Painters(name: "Rembrandt", description: "Rembrandt Harmenszoon van Rijn (July 1606 – October 1669) was a Dutch draughtsman, painter, and printmaker. An innovative and prolific master in three media, he is generally considered one of the greatest visual artists in the history of art and the most important in Dutch art history. Unlike most Dutch masters of the 17th century, Rembrandt's works depict a wide range of style and subject matter, from portraits and self-portraits to landscapes, genre scenes, allegorical and historical scenes, and biblical and mythological themes as well as animal studies. His contributions to art came in a period of great wealth and cultural achievement that historians call the Dutch Golden Age, when Dutch art (especially Dutch painting), although in many ways antithetical to the Baroque style that dominated Europe, was extremely prolific and innovative and gave rise to important new genres. Like many artists of the Dutch Golden Age, such as Jan Vermeer of Delft, Rembrandt was also an avid art collector and dealer.", era: "Baroque"),
+    Painters(name: "Robert Henri", description: "Robert Henri June 24, 1865 – July 12, 1929) was an American painter and teacher. He was a leading figure of the Ashcan School of American realism and an organizer of the group known as \"The Eight,\" a loose association of artists who protested the restrictive exhibition practices of the powerful, conservative National Academy of Design.", era: "Realism")
+    ]
+}
